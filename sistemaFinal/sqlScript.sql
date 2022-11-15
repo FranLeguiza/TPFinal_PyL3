65eefe7982475fb6b7d1c03179aeeb0f0205bd0a
Franco Maximiliano Leguiza <104541449+FranLeguiza@users.noreply.github.com>
2022-11-15 01:38:25 -0300

    Agrego script sql

---
+++ sistemaFinal/sqlScript.sql
@@ -0,0 +1,23 @@
+CREATE table ubicacion (
+id serial primary key, 
+hospital varchar(80),
+nombre varchar (80)
+);
+
+create table habitacion (
+id serial primary key, 
+numero integer
+);
+
+
+create table cama (
+id serial primary key,
+estado varchar (60)
+);
+
+
+create table internacion (
+	id serial primary key,
+	paciente varchar(80) not null,
+	diagnostico varchar(100) not null
+);
